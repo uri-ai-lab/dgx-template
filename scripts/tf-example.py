c99416bc67d5caa7c6825065a91ee9fb5d0eb90f
Harrison Dekker <hdekker@uri.edu>
2019-03-13 16:30:30 -0400

    rename script

---
+++ scripts/tf-example.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+
+# Initialize two constants
+x1 = tf.constant([1,2,3,4])
+x2 = tf.constant([5,6,7,8])
+
+# Multiply
+result = tf.multiply(x1, x2)
+
+# Print the result
+print(result)
